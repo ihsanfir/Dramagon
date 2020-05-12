29d0cd36f54015eed96af79056089f8dbbd9f559
rayhanendra <48467097+rayhanendra@users.noreply.github.com>
2020-05-12 13:20:56 +0700

    CREATE script.js, CREATE modal for thread-img

---
+++ ini js/script.js
@@ -0,0 +1,37 @@
+/*pencet share*/
+
+function myFunction() {
+var x = document.getElementById("share-drop");
+if (x.style.display === "none") {
+  x.style.display = "block";
+} else {
+  x.style.display = "none";
+}
+}
+
+/*modal thread-img*/
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var img = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+img.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
