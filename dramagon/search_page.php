f12976122c63d8abf974da246f9fa747044a443a
rayhanendra <48467097+rayhanendra@users.noreply.github.com>
2020-05-17 23:59:54 +0700

    CREATE favicon

--- dramagon/search_page.php
+++ dramagon/search_page.php
@@ -30,6 +30,11 @@ if (isset($_POST["cari"])) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" type="text/css" href="../style/style.css?v=<?php echo time(); ?>" />
   <link rel="stylesheet" type="text/css" href="../style/sidebar nav.css?v=<?php echo time(); ?>" />
+
+  <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+  <link rel="manifest" href="/favicon/site.webmanifest">
 </head>
 
 <body>
