9b4558014b5df82abe16da81f1bf5cd4c0abd92b
rayhanendra <48467097+rayhanendra@users.noreply.github.com>
2020-05-19 22:49:43 +0700

    Update Font

--- index.php
+++ index.php
@@ -30,6 +30,7 @@ $res_info = mysqli_query($conn, "SELECT * FROM informasi WHERE status='disetujui
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
+    
   </head>
 
 <body>
